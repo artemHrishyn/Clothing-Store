8f7892541c1c370ffc3e973e6838a5b35f389ee3
Artem Hrishyn <artem.hrishyn.it@gmail.com>
2023-12-02 01:29:12 +0200

    edit component login-form and style

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { GoToUrlService } from '../../services/go-to-url.service';
 
 @Component({
   selector: 'csa-login-form',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrl: './login-form.component.scss'
 })
 export class LoginFormComponent {
+  @Input() isLogin: boolean = false;
+  @Output() closeLogin: EventEmitter<boolean> = new EventEmitter<boolean>();
 
+  @Input() imgLogin: string = '';
+  @Output() returnImgLogin: EventEmitter<string> = new EventEmitter<string>();
+
+  public loginData: FormGroup;
+  
+  
+  constructor(private goToUrlService: GoToUrlService) {
+    this.loginData = new FormGroup({
+      login: new FormControl("", Validators.required),
+      password: new FormControl("", Validators.required)
+    })
+  }
+  SignOut() {
+    this.goToUrlService.goToUrl('main');
+    this.isLogin = !this.isLogin
+    this.closeLogin.emit(this.isLogin);
+  }
+  
+   onSubmit(form: FormGroup) {
+     if (form.valid) {
+       if (form.value.login == 'admin' && form.value.password == 'admin')
+       {
+         this.goToUrlService.goToUrl('personal-area');
+         this.imgLogin = 'https://mahesh.orgfree.com/img/log.png';
+         this.closeLogin.emit(this.isLogin);
+         this.returnImgLogin.emit(this.imgLogin);
+       }
+       else {
+         form.reset();
+       }
+    }
+  }
 }
--- src/app/components/login-form/login-form.module.ts
+++ src/app/components/login-form/login-form.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginFormComponent } from './login-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +10,9 @@ import { LoginFormComponent } from './login-form.component';
     LoginFormComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports:[
     LoginFormComponent
