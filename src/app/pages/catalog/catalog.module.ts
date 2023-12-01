83b8322280804003d693176ff7a8502c4b14263e
Artem Hrishyn <artem.hrishyn.it@gmail.com>
2023-12-02 01:39:19 +0200

    add pages catalog and module

---
+++ src/app/pages/catalog/catalog.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CatalogComponent } from './catalog.component';
+
+
+
+@NgModule({
+  declarations: [
+    CatalogComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CatalogModule { }
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CatalogModule } from './catalog/catalog.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CatalogModule
+  ]
+})
+export class PagesModule { }
