fdcc7fbc64f3bd537617fff838f0ee1a1ac0e36f
Artem Hrishyn <artem.hrishyn.it@gmail.com>
2023-12-02 11:33:27 +0200

    add module admin-panel and component all-product-admin

---
+++ src/app/components/admin-panel/admin-panel.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AllProductAdminModule } from './all-product-admin/all-product-admin.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AllProductAdminModule
+  ]
+})
+export class AdminPanelModule { }
---
+++ src/app/components/admin-panel/all-product-admin/all-product-admin.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AllProductAdminComponent } from './all-product-admin.component';
+
+
+
+@NgModule({
+  declarations: [
+    AllProductAdminComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    AllProductAdminComponent
+  ]
+})
+export class AllProductAdminModule { }
