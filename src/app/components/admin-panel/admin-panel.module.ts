755f57848c1024f0fa0dd229249c97a24b4be6f4
Artem Hrishyn <artem.hrishyn.it@gmail.com>
2023-12-02 11:47:40 +0200

    add component new product

--- src/app/components/admin-panel/admin-panel.module.ts
+++ src/app/components/admin-panel/admin-panel.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AllProductAdminModule } from './all-product-admin/all-product-admin.module';
 import { ItemProductAdminModule } from './item-product-admin/item-product-admin.module';
+import { NewProductModule } from './new-product/new-product.module';
 
 
 
@@ -10,7 +11,8 @@ import { ItemProductAdminModule } from './item-product-admin/item-product-admin.
   imports: [
     CommonModule,
     AllProductAdminModule,
-    ItemProductAdminModule
+    ItemProductAdminModule,
+    NewProductModule
   ]
 })
 export class AdminPanelModule { }
---
+++ src/app/components/admin-panel/new-product/new-product.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'csa-new-product',
+  templateUrl: './new-product.component.html',
+  styleUrl: './new-product.component.scss'
+})
+export class NewProductComponent {
+
+}
