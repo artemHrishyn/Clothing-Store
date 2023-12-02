d1453c8c6a1c81e33907dde224d98f694628ed81
Artem Hrishyn <artem.hrishyn.it@gmail.com>
2023-12-02 10:54:45 +0200

    style component footer and edit

--- src/app/components/main-block/footer/footer.component.ts
+++ src/app/components/main-block/footer/footer.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { GoToUrlService } from '../../../services/go-to-url.service';
 
 @Component({
   selector: 'csa-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: [
+    './footer.component.scss',
+    './media.scss'
+  ]
 })
 export class FooterComponent {
+  
+  constructor(
+    private goToUrlService: GoToUrlService
+  ){}
 
+  list = {
+    info: [
+      "Україна м. Одеса",
+      "Про нас",
+      "Повернення та відшкодування"
+    ],
+    help: [
+      "Мій аккаунт",
+      "Безкоштовна доставка до додому",
+      "Статус замовлення"
+    ]
+  }
+
+  public goToUrl(value: string) {
+    this.goToUrlService.goToUrl(value);
+  }
 }
