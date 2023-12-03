36c1fa856692c619d2dec00a80ec319edb141f74
Artem Hrishyn <artem.hrishyn.it@gmail.com>
2023-12-04 00:26:40 +0200

    add pipe delete-item

--- src/app/pipe/delate-item/delete-item.pipe.ts
+++ src/app/pipe/delate-item/delete-item.pipe.ts
@@ -8,4 +8,5 @@ export class DeleteItemPipe implements PipeTransform {
   transform<T>(value: T, data: T[]): T[] {
     return data.filter((element) => element !== value);
   }
+
 }
--- src/app/pipe/pipes.module.ts
+++ src/app/pipe/pipes.module.ts
@@ -3,16 +3,16 @@ import { CommonModule } from '@angular/common';
 import { MixElementsModule } from './mix-elements/mix-elements.service';
 import { CounterModule } from './counter/counter.module';
 import { UniqueValueModule } from './unique-value/unique-value.module';
-import { DeleteItemModule } from './delete-item/delete-item.module';
-
+import { DelateItemModule } from './delate-item/delate-item.module';
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MixElementsModule,
     CounterModule,
-    UniqueValueModule
-    DeleteItemModule
+    UniqueValueModule,
+    DelateItemModule,
+    CounterModule
   ]
 })
-export class PipeModule { }
+export class PipesModule { }
