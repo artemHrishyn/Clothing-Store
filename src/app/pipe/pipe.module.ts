11efd8b3c12791283c8b2ebb1745b37fd81904dc
Artem Hrishyn <artem.hrishyn.it@gmail.com>
2023-12-03 23:53:34 +0200

    add pipe delete-item

--- src/app/pipe/pipe.module.ts
+++ src/app/pipe/pipe.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MixElementsModule } from './mix-elements/mix-elements.service';
 import { CounterModule } from './counter/counter.module';
+import { DeleteItemModule } from './delete-item/delete-item.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MixElementsModule,
-    CounterModule
+    CounterModule,
+    DeleteItemModule
   ]
 })
 export class PipeModule { }
